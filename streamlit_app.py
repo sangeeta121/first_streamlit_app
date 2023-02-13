9f1f376c1cd6f93bbe70f18d2d81747629644547
sangeeta121 <125242867+sangeeta121@users.noreply.github.com>
2023-02-13 16:42:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 streamlit.title('My Parents Healthy Diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🥣Omega 3 and blueberry Oatmeal')
-streamlit.text('🥗Kale,Spinach Rocket Smoothie')
-streamlit.text('Hard-Boild Free_Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.text(' 🥗Kale,Spinach Rocket Smoothie')
+streamlit.text('🐔Hard-Boild Free_Range Egg')
+streamlit.text(' 🥑🍞Avocado Toast')
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
