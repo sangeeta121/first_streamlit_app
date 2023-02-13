83f2bc98fe4ac6d81d1091f5530cefeeabc21ec0
sangeeta121 <125242867+sangeeta121@users.noreply.github.com>
2023-02-13 15:28:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.text('Omega 3 and blueberry Oatmeal')
 streamlit.text('Kale,Spinach Rocket Smoothie')
 streamlit.text('Hard-Boild Free_Range Egg')
 streamlit.text('Avocado Toast')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
