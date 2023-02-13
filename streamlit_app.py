c8c0aa64eb5c7b2ba0cde13d5cb07343edfdad02
sangeeta121 <125242867+sangeeta121@users.noreply.github.com>
2023-02-13 15:48:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 # let put's pick list here so they can pick the fruit they want to include
-streamlit.multiselect("pick some fruit",list(my_fruit_list))
+streamlit.multiselect("pick some fruit",list(my_fruit_list.index))
 #display the table on the page
-streamlit.dataframe(my_fruit_list.index)
+streamlit.dataframe(my_fruit_list)
