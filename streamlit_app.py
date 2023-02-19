f862e5f6dec1df25a7e89cf09f09655b67751dda
sangeeta121 <125242867+sangeeta121@users.noreply.github.com>
2023-02-19 15:24:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 
 import streamlit
 streamlit.title('My Parents Healthy Diner')
-streamlit.header('hii helo')
+streamlit.header('Breakfast menu')
+streamlit.text('??Omega 3 and blueberry Oatmeal')
+streamlit.text(' ??Kale,Spinach Rocket Smoothie')
+streamlit.text('??Hard-Boild Free_Range Egg')
+streamlit.text(' ????Avocado Toast')
+streamlit.header('???? Build Your Own Fruit Smoothie ????')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected=streamlit.multiselect("pick some fruit",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
 
 import snowflake.connector
