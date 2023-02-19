ee1fe2f4f84cb14ef398baec9c5f0cfde8b488f8
sangeeta121 <125242867+sangeeta121@users.noreply.github.com>
2023-02-19 20:17:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ try :
      else :
           back_from_function =   get_fruityvice_data(fruit_choice)
           streamlit.dataframe(back_from_function)
-           streamlit.write('The user entered ', fruit_choice)
-           streamlit.write('Thanks for adding',fruit_choice)
+           #streamlit.write('The user entered ', fruit_choice)
+           #streamlit.write('Thanks for adding',fruit_choice)
           
                                                     
       
